0ef64a6d161a64b18977e26f2dbb119d5b40b46f
K <15225129190@163.com>
2023-03-10 10:24:17 +0800

    初次部署

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Austin's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_TITLE = `倒立的漏斗`;
+export const SITE_DESCRIPTION = '增加自己的深度，延伸自己的广度';
+export const SITE_EMAIL = '15225129190@163.com'
+export const SITE_NAME = 'spilar.cn';
+export const SITE_URL = "https://spilar.cn";
