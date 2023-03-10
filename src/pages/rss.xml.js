74de45f9b8b4b405c4ea63a747bea1af9fa57457
K <15225129190@163.com>
2023-03-10 10:45:04 +0800

    初次部署

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,9 +2,9 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function get() {
   return rss({
-    title: "Austin's Blog",
-    description: "Site description",
-    site: 'https://astro-blog.qum.cc',
+    title: "spilar's Blog",
+    description: "分享读过的书、去过的地方，还有其他有趣的事情。",
+    site: 'https://spilar.cn',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>zh-cn</language>`,
   });
