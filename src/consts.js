2693cc7088eaa213aff534d2918570be9ece509c
K <15225129190@163.com>
2023-03-13 15:31:32 +0800

    优化隐藏文章的方式

--- src/consts.js
+++ src/consts.js
@@ -3,3 +3,4 @@ export const SITE_DESCRIPTION = '增加自己的深度，延伸自己的广度';
 export const SITE_EMAIL = '15225129190@163.com'
 export const SITE_NAME = 'spilar.cn';
 export const SITE_URL = "https://spilar.cn";
+export const POSTS_HIDDEN = ["书签"];
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,14 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { POSTS_HIDDEN } from "../consts";
 
 export async function get() {
+  let allPost = await pagesGlobToRssItems(import.meta.glob('./**/*.md'));
+  let items = allPost.filter(t => POSTS_HIDDEN.indexOf(t.title) === -1);
   return rss({
     title: "spilar's Blog",
     description: "分享读过的书、去过的地方，还有其他有趣的事情。",
     site: 'https://spilar.cn',
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items,
     customData: `<language>zh-cn</language>`,
   });
 }
